9729a86516519b82997dce5798da2dc881b5ac92
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-02-03 16:16:02 -0300

    :sparkles: feat: list all providers

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,27 +1,107 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 import {useAuth} from '../../hooks/Auth';
 
-import {Container, Header, HeaderTitle, UserName, UserPic} from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  UserAvatar,
+  ProfileButton,
+  Title,
+  ProviderList,
+  WrapperProvider,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderSchedule,
+  ScheduleIcon,
+  ScheduleText,
+} from './styles';
+import api from '../../services/api';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  const {signOut} = useAuth();
+  const {user} = useAuth();
+  const {navigate} = useNavigation();
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('/providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointment', {providerId});
+    },
+    [navigate],
+  );
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
     <Container>
       <Header>
         <HeaderTitle>
           Bem vindo, {'\n'}
-          <UserName>Héliton Oliveira</UserName>
+          <UserName>{user.name}</UserName>
         </HeaderTitle>
 
-        <UserPic
-          source={{
-            uri:
-              'https://avatars.githubusercontent.com/u/45343619?s=460&u=1929279b8eb8dbe2434c20e7ad7e239674b4cc17&v=4',
-          }}
-        />
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar
+            source={{
+              uri: user.avatar_url,
+            }}
+          />
+        </ProfileButton>
       </Header>
+
+      <ProviderList
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        ListHeaderComponent={<Title>Cabeleleiros</Title>}
+        renderItem={({item: provider}) => (
+          <WrapperProvider
+            onPress={() => navigateToCreateAppointment(provider.id)}>
+            <ProviderAvatar
+              source={{
+                uri:
+                  'https://avatars.githubusercontent.com/u/45343619?s=460&u=1929279b8eb8dbe2434c20e7ad7e239674b4cc17&v=4',
+              }}
+            />
+
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+
+              <ProviderSchedule>
+                <ScheduleIcon name="calendar" />
+                <ScheduleText>Segunda à sexta</ScheduleText>
+              </ProviderSchedule>
+
+              <ProviderSchedule>
+                <ScheduleIcon name="clock" />
+                <ScheduleText>8h às 17h</ScheduleText>
+              </ProviderSchedule>
+            </ProviderInfo>
+          </WrapperProvider>
+        )}
+      />
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,11 +1,18 @@
 import styled, {css} from 'styled-components/native';
+import {FlatList} from 'react-native';
+import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+import {Provider} from './index';
+import {RectButton} from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
 `;
 
 export const Header = styled.View`
-  padding: 56px 24px 24px;
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
   background: ${({theme}) => theme.colors.blackMedium};
 
   flex-direction: row;
@@ -30,8 +37,75 @@ export const UserName = styled.Text`
   `}
 `;
 
-export const UserPic = styled.Image`
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
   width: 56px;
   height: 56px;
   border-radius: 28px;
+`;
+
+export const Title = styled.Text`
+  ${({theme}) => css`
+    margin-bottom: 24px;
+    font-size: 25px;
+    font-family: ${theme.fonts['RobotoSlab-Regular']};
+    color: ${theme.colors.white};
+  `}
+`;
+
+export const ProviderList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px 24px;
+`;
+
+export const WrapperProvider = styled(RectButton)`
+  flex-direction: row;
+  align-items: center;
+
+  padding: 20px 16px;
+  border-radius: 10px;
+  margin-bottom: 16px;
+  background: ${({theme}) => theme.colors.shape};
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+`;
+
+export const ProviderInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const ProviderName = styled.Text`
+  ${({theme}) => css`
+    font-size: 18px;
+    font-family: ${theme.fonts['RobotoSlab-Medium']};
+    color: ${theme.colors.white};
+    margin-bottom: 6px;
+  `}
+`;
+
+export const ProviderSchedule = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 6px;
+`;
+
+export const ScheduleIcon = styled(FeatherIcon).attrs({
+  size: 12,
+})`
+  color: ${({theme}) => theme.colors.orange};
+  margin-right: 12px;
+`;
+
+export const ScheduleText = styled.Text`
+  ${({theme}) => css`
+    font-size: 12px;
+    line-height: 16px;
+    font-family: ${theme.fonts['RobotoSlab-Regular']};
+    color: ${theme.colors.gray};
+  `}
 `;
