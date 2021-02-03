c08b4678818f63839531b28a7a77b236562f5cbf
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-02-03 12:16:20 -0300

    :recycle: chore: adjust user interface

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -9,9 +9,16 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredenctial {
@@ -20,7 +27,7 @@ interface SignInCredenctial {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredenctial): Promise<void>;
   signOut(): void;
