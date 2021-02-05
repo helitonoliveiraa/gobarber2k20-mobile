6e79adc5afce257d329f6b6e87a83f128fe79a72
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-02-05 18:22:37 -0300

    :wrench: feat: profile style

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,208 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useRef, useCallback} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+} from 'react-native';
+import {Form} from '@unform/mobile';
+import {FormHandles} from '@unform/core';
+import * as Yup from 'yup';
 
-const Profile: React.FC = () => {
-  return <View />;
+import getValidationErrors from '../../utils/validationErros';
+import api from '../../services/api';
+
+import Input from '../../components/Input';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  ButtonBorderless,
+  Icon,
+  AvatarContainer,
+  Avatar,
+  WrapperPicIcon,
+  SaveButton,
+} from './styles';
+import {useAuth} from '../../hooks/Auth';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const {goBack} = useNavigation();
+  const {user, signOut} = useAuth();
+
+  const formRef = useRef<FormHandles>(null);
+  const emailInputRef = useRef<TextInput>(null);
+  const oldPasswordRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const ConfirmPasswordRef = useRef<TextInput>(null);
+
+  const handleSignUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail é obrigatório'),
+        password: Yup.string()
+          .required('Senha é obrigatória')
+          .min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, {abortEarly: false});
+
+      // await api.post('/users', data);
+
+      Alert.alert(
+        'Cadastro realizado!',
+        'Vocẽ já pode fazer seu logon no GoBarber!',
+      );
+
+      // navigation.goBack();
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      Alert.alert(
+        'Erro no cadastro!',
+        'Ocorreu um erro ao fazer cadastro, tente novamente.',
+      );
+    }
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{flex: 1}}>
+          <Container>
+            <Header>
+              <ButtonBorderless onPress={() => goBack()}>
+                <Icon name="chevron-left" />
+              </ButtonBorderless>
+
+              <HeaderTitle>Meu perfil</HeaderTitle>
+
+              <ButtonBorderless onPress={signOut}>
+                <Icon name="power" />
+              </ButtonBorderless>
+            </Header>
+
+            <AvatarContainer>
+              <Avatar
+                source={{
+                  uri:
+                    'https://avatars.githubusercontent.com/u/45343619?s=460&u=1929279b8eb8dbe2434c20e7ad7e239674b4cc17&v=4',
+                }}
+              />
+
+              <WrapperPicIcon>
+                <Icon name="camera" />
+              </WrapperPicIcon>
+            </AvatarContainer>
+
+            <Form
+              style={{marginHorizontal: 20}}
+              ref={formRef}
+              onSubmit={handleSignUp}
+              initialData={{
+                name: user.name,
+                email: user.email,
+              }}>
+              <Input
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                autoCapitalize="words"
+                returnKeyType="next"
+                onEndEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={emailInputRef}
+                name="email"
+                icon="mail"
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                textContentType="newPassword"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  oldPasswordRef.current?.focus();
+                }}
+              />
+
+              <Input
+                containerStyle={{marginTop: 32}}
+                ref={oldPasswordRef}
+                name="olg_password"
+                icon="lock"
+                placeholder="Senha atual"
+                secureTextEntry
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Nova senha"
+                secureTextEntry
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  ConfirmPasswordRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={ConfirmPasswordRef}
+                name="password_confirmation"
+                icon="lock"
+                placeholder="Confiramr senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+
+              <SaveButton
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}>
+                Confirmar mudanças
+              </SaveButton>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
 
-export default Profile;
+export default SignUp;
---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,92 @@
+import styled, {css} from 'styled-components/native';
+import {Platform} from 'react-native';
+import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import {BorderlessButton, RectButton} from 'react-native-gesture-handler';
+
+import Button from '../../components/Button';
+
+interface IconProps {
+  name: 'chevron-left' | 'power' | 'camera';
+}
+
+const paddingTop = css`
+  padding-top: ${Platform.OS === 'ios'
+    ? getStatusBarHeight() + 32
+    : getStatusBarHeight()}px;
+`;
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  margin: 0 24px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  ${paddingTop}
+  margin-bottom: 32px;
+`;
+
+export const HeaderTitle = styled.Text`
+  ${({theme}) => css`
+    font-size: 20px;
+    font-family: ${theme.fonts['RobotoSlab-Medium']};
+    color: ${theme.colors.white};
+  `}
+`;
+
+export const ButtonBorderless = styled(BorderlessButton)``;
+
+export const AvatarContainer = styled.View`
+  width: 186px;
+  height: 186px;
+  align-self: center;
+  margin-bottom: 32px;
+  position: relative;
+`;
+
+export const Avatar = styled.Image`
+  width: 186px;
+  height: 186px;
+  border-radius: 93px;
+`;
+
+export const WrapperPicIcon = styled(RectButton)`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  background: ${({theme}) => theme.colors.orange};
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  right: 4px;
+  bottom: 4px;
+`;
+
+export const Icon = styled(FeatherIcon).attrs({
+  size: 20,
+})<IconProps>`
+  ${({theme, name}) => css`
+    ${name === 'camera' &&
+    css`
+      color: ${theme.colors.background};
+    `};
+
+    ${name === 'chevron-left' &&
+    css`
+      color: ${theme.colors.gray};
+    `};
+
+    ${name === 'power' &&
+    css`
+      color: ${theme.colors.error};
+      opacity: 0.8;
+    `};
+  `}
+`;
+
+export const SaveButton = styled(Button)`
+  margin: 72px 0 24px;
+`;
